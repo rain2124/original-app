b500066b32847399d43fb32415fc66c9ff734e73
rain2124 <bosu1305@gmail.com>
2025-03-12 18:13:08 +0900

    imageエラー修正

--- crypto-app/next.config.ts
+++ crypto-app/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  images: {
+    domains: ['platform.theverge.com', 'media.wired.com','gizmodo.com'],
+  },
 };
 
 export default nextConfig;
